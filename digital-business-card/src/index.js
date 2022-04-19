66c99691bce90fe2aaff2ae75b71832f0c8a88cf
Hayley Wood <onlinecowork@gmail.com>
2022-04-19 10:48:09 +1000

    set-up folders

---
+++ digital-business-card/src/index.js
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/clients";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
