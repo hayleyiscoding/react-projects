860ae03660ab7f607c824bbabac2497e00ff931a
Hayley Wood <onlinecowork@gmail.com>
2022-04-24 20:56:57 +1000

    edit header text

--- travel-journal/travel-journal/src/components/Header.js
+++ travel-journal/travel-journal/src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <header className='header text-center container'>
       <a href='{#}' className='header--link flex justify-center align-center'>
         <img src={worldIcon} alt='logo' className='header--icon' />
-        <h1 className='header--title'>mytraveljournal.</h1>
+        <h1 className='header--title'>my travel journal.</h1>
       </a>
     </header>
   );
